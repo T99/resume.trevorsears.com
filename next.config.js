b2a530cd74112d6651715d963d3e7135706964e8
Trevor Sears <trevor@trevorsears.com>
2022-08-17 17:04:59 -0400

    Added SVGR for native SVG support.

--- next.config.js
+++ next.config.js
@@ -1,10 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
-  compiler: {
-    emotion: true,
-  }
-}
+	reactStrictMode: true,
+	swcMinify: true,
+	compiler: {
+		emotion: true,
+	},
+	webpack: (config) => {
+		config.module.rules.push({
+			test: /\.svg$/,
+			use: ["@svgr/webpack"],
+		});
+		return config;
+	},
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
