38e2ad968976154dc872aa8ba0037f06f0932f2d
Trevor Sears <trevor@trevorsears.com>
2022-08-17 17:05:53 -0400

    Split some `CONTACT_INFORMATION` out into new `FIND_ME_ONLINE` const.

--- data/contact-information-object.ts
+++ data/contact-information-object.ts
@@ -5,13 +5,16 @@
  */
 
 import githubIcon from "../assets/icons/github.svg";
+import layersIcon from "../assets/icons/layers.svg";
+import linkedinIcon from "../assets/icons/linkedin.svg";
 import mailIcon from "../assets/icons/mail.svg";
 import mapPinIcon from "../assets/icons/map-pin.svg";
 import phoneIcon from "../assets/icons/phone.svg";
+import { ReactElement } from "react";
 
 export type ContactInformationObject = {
 	name: string,
-	icon: string,
+	icon: ReactElement,
 	content: string,
 	link: string,
 };
@@ -30,15 +33,30 @@ export const CONTACT_INFORMATION: ContactInformationObject[] = [
 		link: "mailto:trevor@trevorsears.com",
 	},
 	{
-		name: "GitHub Account",
-		icon: githubIcon,
-		content: "github.com/T99",
-		link: "https://github.com/T99",
-	},
-	{
 		name: "Location",
 		icon: mapPinIcon,
 		content: "Traverse City, Michigan",
 		link: "https://goo.gl/maps/u5sLKDSg5f3Z3fpf8",
 	}
 ];
+
+export const FIND_ME_ONLINE: ContactInformationObject[] = [
+	{
+		name: "GitHub Account",
+		icon: githubIcon,
+		content: "github.com/T99",
+		link: "https://github.com/T99",
+	},
+	{
+		name: "Stack Overflow Account",
+		icon: layersIcon,
+		content: "stackoverflow.com/users/trevor-sears",
+		link: "https://stackoverflow.com/users/4997194/trevor-sears"
+	},
+	{
+		name: "LinkedIn Account",
+		icon: linkedinIcon,
+		content: "linkedin.com/in/trevortsears",
+		link: "https://www.linkedin.com/in/trevortsears/"
+	}
+]
